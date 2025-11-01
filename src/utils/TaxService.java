fbe25d7bfcfd2a8fa7405cf400cadefd838c4316
SBEWill <williandiniz2412@hotmail.com>
2025-11-01 14:38:01 -0300

    atualizacao gitinore

--- src/utils/TaxService.java
+++ src/utils/TaxService.java
@@ -4,15 +4,33 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class TaxService {
-    private final String apiKey = "11ba23a3d20fe597f293c4fe";
+    private final String apiKey;
+
+    public TaxService() {
+        this.apiKey = loadApiKey();
+    }
+    private String loadApiKey() {
+        Properties prop = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            prop.load(input);
+            return prop.getProperty("API_KEY");
+        } catch (IOException ex) {
+            System.err.println("ERRO: Não foi possível carregar a chave de API do arquivo config.properties.");
+            System.err.println("Certifique-se de que o arquivo config.properties existe e está no diretório correto.");
+
+            throw new RuntimeException("Chave de API ausente ou inacessível.", ex);
+        }
+    }
 
     public BigDecimal obterTaxa(String fromCode, String toCode) throws IOException, InterruptedException {
 
